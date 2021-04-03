d63903f18ab59755aefe85f27ddc93b636b34a5d
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2021-04-03 14:07:20 +0100

    clean: Refactor get_first_paragraph as static method of MetaDescription

--- mkdocs_meta_descriptions_plugin/plugin.py
+++ mkdocs_meta_descriptions_plugin/plugin.py
@@ -6,16 +6,6 @@ from mkdocs.plugins import BasePlugin
 from bs4 import BeautifulSoup
 
 
-def get_first_paragraph(html):
-    # Strip all page content in subsections
-    html = re.split("<h[2-6]", html, maxsplit=1, flags=re.IGNORECASE)[0]
-    # Obtain first paragraph that is direct child of body element
-    first_paragraph = BeautifulSoup(html, features="lxml").select_one("body > p")
-    if first_paragraph is not None:
-        return first_paragraph.get_text()
-    return None
-
-
 class MetaDescription(BasePlugin):
 
     config_scheme = (
@@ -32,7 +22,17 @@ class MetaDescription(BasePlugin):
             pass
         else:
             # Create description based on the first paragraph
-            first_paragraph = get_first_paragraph(html)
+            first_paragraph = self.get_first_paragraph(html)
             if first_paragraph is not None and len(first_paragraph) > 0:
                 page.meta["description"] = first_paragraph
         return html
+
+    @staticmethod
+    def get_first_paragraph(html):
+        # Strip all page content in subsections
+        html = re.split("<h[2-6]", html, maxsplit=1, flags=re.IGNORECASE)[0]
+        # Obtain first paragraph that is direct child of body element
+        first_paragraph = BeautifulSoup(html, features="lxml").select_one("body > p")
+        if first_paragraph is not None:
+            return first_paragraph.get_text()
+        return None
