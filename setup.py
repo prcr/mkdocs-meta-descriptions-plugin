27d771676a12c52c36f81a371d7470b6bfabe3fa
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2021-04-06 15:17:18 +0100

    fix: Sync CI/CD Python version with local development environment

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.6"
     ],
     packages=find_packages(),
     entry_points={
