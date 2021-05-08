5466aad626c2bac829001149a1eaba207cc089c7
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2021-05-08 13:19:06 +0100

    clean: Small changes to export.py

--- mkdocs_meta_descriptions_plugin/export.py
+++ mkdocs_meta_descriptions_plugin/export.py
@@ -12,23 +12,25 @@ logger = logging.getLogger("mkdocs.mkdocs_meta_descriptions_plugin")
 
 
 class Export:
+    """Read meta descriptions from the generated HTML and export them as a CSV file."""
+
     def __init__(self, config, output_file="meta_descriptions.csv"):
-        self.site_dir = config.get("site_dir")
-        self.site_url = config.get("site_url")
-        self.use_directory_urls = config.get("use_directory_urls")
+        self._site_dir = config.get("site_dir")
+        self._site_url = config.get("site_url")
+        self._use_directory_urls = config.get("use_directory_urls")
 
-        self.body_pattern = re.compile("<body", flags=re.IGNORECASE)
-        self.output_path = os.path.join(self.site_dir, output_file)
+        self._body_pattern = re.compile("<body", flags=re.IGNORECASE)
+        self._output_path = os.path.join(self._site_dir, output_file)
 
-        self.pages = None
-        self.meta_descriptions = None
-        if self.site_dir and self.site_url and self.output_path:
-            self.pages = self._parse_sitemap()
-            if self.pages:
-                self.meta_descriptions = self._read_descriptions()
+        self._pages = None
+        self._meta_descriptions = None
+        if self._site_dir and self._site_url and self._output_path:
+            self._pages = self._parse_sitemap()
+            if self._pages:
+                self._meta_descriptions = self._read_descriptions()
 
     def _parse_sitemap(self):
-        sitemap_path = os.path.join(self.site_dir, "sitemap.xml")
+        sitemap_path = os.path.join(self._site_dir, "sitemap.xml")
         if not os.path.isfile(sitemap_path):
             logger.error(PLUGIN_TAG + f"Can't open sitemap {sitemap_path}")
             return None
@@ -41,17 +43,17 @@ class Export:
 
     def _read_descriptions(self):
         meta_descriptions = {}
-        logger.info(PLUGIN_TAG + f"Reading meta descriptions from {len(self.pages)} HTML pages")
+        logger.info(PLUGIN_TAG + f"Reading meta descriptions from {len(self._pages)} HTML pages")
         count = 0
-        for page in self.pages:
-            # Transform URLs in local file names
-            page_path = os.path.join(self.site_dir, unquote(page.url).replace(self.site_url, ""))
-            if self.use_directory_urls:
+        for page in self._pages:
+            # Transform URLs into local file names
+            page_path = os.path.join(self._site_dir, unquote(page.url).replace(self._site_url, ""))
+            if self._use_directory_urls:
                 page_path = os.path.join(page_path, "index.html")
             with open(page_path) as page_file:
                 html = page_file.read()
                 # Strip page body to improve performance
-                html = re.split(self.body_pattern, html, maxsplit=1)[0]
+                html = re.split(self._body_pattern, html, maxsplit=1)[0]
                 soup = BeautifulSoup(html, features="lxml")
                 meta_element = soup.select_one("meta[name=\"description\"]")
                 if meta_element:
@@ -59,16 +61,16 @@ class Export:
                     meta_descriptions[page_path] = meta_element.get("content")
                 else:
                     meta_descriptions[page_path] = ""
-        if count != len(self.pages):
-            logger.warning(PLUGIN_TAG + f"Didn't find meta descriptions for {len(self.pages) - count} HTML pages")
+        if count != len(self._pages):
+            logger.warning(PLUGIN_TAG + f"Didn't find meta descriptions for {len(self._pages) - count} HTML pages")
         return meta_descriptions
 
     def write_csv(self):
-        if self.meta_descriptions and self.meta_descriptions.keys():
-            logger.info(PLUGIN_TAG + f"Writing {self.output_path}")
-            with open(self.output_path, "w") as csv_file:
+        if self._meta_descriptions and self._meta_descriptions.keys():
+            logger.info(PLUGIN_TAG + f"Writing {self._output_path}")
+            with open(self._output_path, "w") as csv_file:
                 csv_writer = csv.writer(csv_file)
-                for page, meta_description in self.meta_descriptions.items():
-                    csv_writer.writerow([os.path.relpath(page, self.site_dir), meta_description])
+                for page, meta_description in self._meta_descriptions.items():
+                    csv_writer.writerow([os.path.relpath(page, self._site_dir), meta_description])
         else:
             logger.error(PLUGIN_TAG + "Can't find meta descriptions to write to CSV file")
