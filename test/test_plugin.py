ab15f89b335900757618664b47c8d0e5dd8e5111
Paulo Ribeiro <paulo@diffraction.pt>
2021-05-08 22:36:11 +0100

    test: Add basic tests for export_csv

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -2,6 +2,7 @@ import pytest
 import glob
 import tempfile
 import os.path
+import filecmp
 
 from mkdocs.__main__ import build_command
 from mkdocs.structure.files import File, Files
@@ -44,63 +45,74 @@ def build(request, use_directory_urls):
                             os.path.join(os.getcwd(), "test/docs/"),
                             tempdir,
                             use_directory_urls[1]) for file in markdown_files_list])
-        yield result, files, mkdocs_yml
+        yield result, files, mkdocs_yml, use_directory_urls[1]
 
 
 class TestPlugin:
     def test_build(self, build):
-        result, _, _ = build
+        result, _, _, _ = build
         assert result.exit_code == 0
 
     def test_build_output(self, build):
-        _, files, _ = build
+        _, files, _, _ = build
         for f in files:
             assert os.path.isfile(f.abs_dest_path)
 
     def test_index_md(self, build):
-        _, files, _ = build
+        _, files, _, _ = build
         expected = (
             "For full documentation visit mkdocs.org."
         )
         assert get_meta_description(files, "index.md") == expected
 
     def test_first_paragraph(self, build):
-        _, files, _ = build
+        _, files, _, _ = build
         expected = (
             "First paragraph."
         )
         assert get_meta_description(files, "first_paragraph.md") == expected
 
     def test_first_paragraph_no_heading(self, build):
-        _, files, _ = build
+        _, files, _, _ = build
         expected = (
             "First paragraph."
         )
         assert get_meta_description(files, "first_paragraph_no_heading.md") == expected
 
     def test_first_paragraph_no_paragraph(self, build):
-        _, files, _ = build
-        expected = (
-            "Value of site_description on mkdocs.yml"
-        )
+        _, files, mkdocs_yml, _ = build
+        if "no_site_description" in mkdocs_yml:
+            expected = (
+                None
+            )
+        else:
+            expected = (
+                "Value of site_description on mkdocs.yml"
+            )
         assert get_meta_description(files, "first_paragraph_no_paragraph.md") == expected
 
     def test_first_paragraph_no_intro(self, build):
-        _, files, _ = build
-        expected = (
-            "Value of site_description on mkdocs.yml"
-        )
+        _, files, mkdocs_yml, _ = build
+        if "no_site_description" in mkdocs_yml:
+            expected = (
+                None
+            )
+        else:
+            expected = (
+                "Value of site_description on mkdocs.yml"
+            )
+
         assert get_meta_description(files, "first_paragraph_no_intro.md") == expected
 
     def test_front_matter_description(self, build):
-        _, files, _ = build
+        _, files, _, _ = build
         expected = (
             "Value of meta description on front_matter_description.md"
         )
         assert get_meta_description(files, "front_matter_description.md") == expected
 
     def test_escape_html_entities(self, build):
-        _, files, _ = build
+        _, files, _, _ = build
         expected = (
             "First paragraph with HTML entities: \"quotes\", 'single quotes', "
             "<greater and less than>, &ampersand&."
@@ -109,10 +121,32 @@ class TestPlugin:
 
 
 class TestExport:
-    def test_no_site_url(self, build):
-        result, _, mkdocs_yml = build
-        if "export_csv_no_site_url" in mkdocs_yml:
-            expected = (
-                "WARNING -  [meta-descriptions] Can't export meta descriptions to CSV because site_url is not defined."
-            )
-            assert expected in result.output
+    def test_export_csv(self, build):
+        _, files, mkdocs_yml, use_directory_urls = build
+        if "export_csv.yml" in mkdocs_yml:
+            index_path = files.get_file_from_path("index.md").abs_dest_path
+            csv_path = index_path.replace("index.html", "meta_descriptions.csv")
+            if use_directory_urls:
+                assert filecmp.cmp("test/meta_descriptions.csv", csv_path)
+            else:
+                assert filecmp.cmp("test/meta_descriptions_no_directory_urls.csv", csv_path)
+
+    def test_export_csv_no_site_description(self, build):
+        _, files, mkdocs_yml, use_directory_urls = build
+        if "export_csv_no_site_description.yml" in mkdocs_yml:
+            index_path = files.get_file_from_path("index.md").abs_dest_path
+            csv_path = index_path.replace("index.html", "meta_descriptions.csv")
+            if use_directory_urls:
+                assert filecmp.cmp("test/meta_descriptions_no_site_description.csv", csv_path)
+            else:
+                assert filecmp.cmp("test/meta_descriptions_no_site_description_no_directory_urls.csv", csv_path)
+
+    def test_export_csv_no_site_url(self, build):
+        result, _, mkdocs_yml, _ = build
+        if "export_csv" in mkdocs_yml:
+            if "no_site_url" in mkdocs_yml:
+                expected = (
+                    "WARNING -  [meta-descriptions] Can't export meta descriptions to CSV "
+                    "because site_url is not defined."
+                )
+                assert expected in result.output
