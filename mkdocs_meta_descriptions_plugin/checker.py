2bbc93110f3a3aaf38e2e8ee7aac208619b534f4
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2022-10-01 17:12:21 +0100

    clean: Add warning for missing meta descriptions

--- mkdocs_meta_descriptions_plugin/checker.py
+++ mkdocs_meta_descriptions_plugin/checker.py
@@ -9,29 +9,30 @@ class Checker:
     _check = False
     _min_length = 50
     _max_length = 160
-    _warning_syntax_length = Template(
+    _template_warning_missing = Template(
+        "Meta description not found: $page")
+    _template_warning_length = Template(
         "Meta description $character_count character$plural $comparative than $limit: $page")
 
     def _check_length(self, page):
         length = len(page.meta.get("description", ""))
         if length == 0:
-            # Skip length check
-            # TODO Check that there's a warning for missing descriptions
-            return
+            logger.write(logger.Warning, self._template_warning_missing.substitute(page=page.file.src_path))
+            return  # Skip length check
         elif length < self._min_length:
             diff = self._min_length - length
-            logger.write(logger.Warning, self._warning_syntax_length.substitute(character_count=diff,
-                                                                                plural="s"[:diff != 1],
-                                                                                comparative="shorter",
-                                                                                limit=self._min_length,
-                                                                                page=page.file.src_path))
+            logger.write(logger.Warning, self._template_warning_length.substitute(character_count=diff,
+                                                                                  plural="s"[:diff != 1],
+                                                                                  comparative="shorter",
+                                                                                  limit=self._min_length,
+                                                                                  page=page.file.src_path))
         elif length > self._max_length:
             diff = length - self._max_length
-            logger.write(logger.Warning, self._warning_syntax_length.substitute(character_count=diff,
-                                                                                plural="s"[:diff != 1],
-                                                                                comparative="longer",
-                                                                                limit=self._max_length,
-                                                                                page=page.file.src_path))
+            logger.write(logger.Warning, self._template_warning_length.substitute(character_count=diff,
+                                                                                  plural="s"[:diff != 1],
+                                                                                  comparative="longer",
+                                                                                  limit=self._max_length,
+                                                                                  page=page.file.src_path))
 
     def initialize(self, config):
         self._check = config.get("enable_checks")
