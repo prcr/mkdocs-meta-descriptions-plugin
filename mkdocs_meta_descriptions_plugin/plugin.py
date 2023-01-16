15f8195d9c1030f968b3b9329f63af2bc776a17a
Paulo Ribeiro <paulo@diffraction.pt>
2023-01-16 10:55:36 +0000

    test: Trigger coverage summary

--- mkdocs_meta_descriptions_plugin/plugin.py
+++ mkdocs_meta_descriptions_plugin/plugin.py
@@ -45,6 +45,28 @@ class MetaDescription(BasePlugin):
         return config
 
     def on_page_content(self, html, page, config, files):
+        if False:
+            logger.write(logger.Debug, "Hello world!")
+            logger.write(logger.Debug, "Hello world!")
+            logger.write(logger.Debug, "Hello world!")
+            logger.write(logger.Debug, "Hello world!")
+            logger.write(logger.Debug, "Hello world!")
+        if page.meta.get("description", None):
+            # Skip pages that already have an explicit meta description
+            self._count_meta += 1
+            logger.write(logger.Debug, f"Adding meta description from front matter: {page.file.src_path}")
+        else:
+            # Create meta description based on the first paragraph of the page
+            first_paragraph_text = self._get_first_paragraph_text(html)
+            if len(first_paragraph_text) > 0:
+                page.meta["description"] = first_paragraph_text
+                self._count_first_paragraph += 1
+                logger.write(logger.Debug, f"Adding meta description from first paragraph: {page.file.src_path}")
+            else:
+                self._count_empty += 1
+        return html
+
+    def test(self, html, page, config, files):
         if page.meta.get("description", None):
             # Skip pages that already have an explicit meta description
             self._count_meta += 1
