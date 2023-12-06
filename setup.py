62e934c8b0d844a09f8fb5ab646db773c1690820
Paulo Ribeiro <paulo@diffraction.pt>
2023-12-06 15:29:27 +0000

    feat: Add Python 3.12 support

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11"
+        "Programming Language :: Python :: 3.12"
     ],
     packages=["mkdocs_meta_descriptions_plugin"],
     entry_points={
